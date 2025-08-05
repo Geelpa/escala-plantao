acd1527bde6f528b5e1a1994d1356c85bf5848de
Geelpa <eduardo.augusto157@hotmail.com>
2025-08-05 19:39:02 -0300

    '

--- js/main.js
+++ js/main.js
@@ -1,26 +1,20 @@
 import { renderCalendar, mudarMes } from "./calendar.js";
 import { loadEmployees } from "./employees.js";
 import { loadUpcomingSchedulesDay } from "./schedule.js";
-import { db } from "./firebase-config.js";
 import {
     collection, onSnapshot
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-<<<<<<< Updated upstream
-=======
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { firebaseConfig, db } from "./firebase-config.js";
+import { db, app, firebaseConfig } from "./firebase-config.js";
 
 
-const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 onAuthStateChanged(auth, user => {
     if (!user) {
-        // Redireciona para login se não estiver autenticado
         window.location.href = "login.html";
     } else {
-        // Usuário autenticado — carrega o calendário e funcionalidades
         iniciarAplicacao();
     }
 });
@@ -36,17 +30,6 @@ function iniciarAplicacao() {
     loadEmployees();
     loadUpcomingSchedulesDay();
 }
->>>>>>> Stashed changes
-
-const prevMonth = document.getElementById("prevMonth");
-const nextMonth = document.getElementById("nextMonth");
-
-
-<<<<<<< Updated upstream
-prevMonth.addEventListener("click", () => mudarMes(-1));
-nextMonth.addEventListener("click", () => mudarMes(1));
-=======
->>>>>>> Stashed changes
 
 export function escutarEscalasTempoReal() {
     const ref = collection(db, "schedules");
@@ -63,7 +46,3 @@ export function escutarEscalasTempoReal() {
         renderCalendar(scales);
     });
 }
-
-loadEmployees();
-loadUpcomingSchedulesDay();
-escutarEscalasTempoReal();
